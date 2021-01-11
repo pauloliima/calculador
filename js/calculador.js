e4ebf157b9512364f4d9ade7c79ac171db91cdac
Paulo Lima <paulo.slima@aluno.faculdadeimpacta.com.br>
2021-01-11 18:04:23 -0300

    Att
    Atualizado com o codigo do stackoverflow

--- js/calculador.js
+++ js/calculador.js
@@ -1,34 +1,64 @@
-let medidas = {media1: 1, medida2: 0.6}
+var inputs;
+var medidas = [
+    { value: 1, amount: 0 },
+    { value: 0.6, amount: 0 },
+    { value: 1.1, amount: 0 },
+    { value: 0.2, amount: 0 },
+    { value: 0.5, amount: 0 }
+]
 
-function calc_elementos() {
-  const elements = document.querySelectorAll('.medida1');
-  const elements2 = document.querySelectorAll('.medida2');
-
-  let total1 = 0;
-  let total2 = 0;  
-  
-  let soma = 0;
-  let n1 = 0;
-  let n2 = 0;
-
-  Array.from(elements).forEach((element, index) => {
-    n1 += parseInt(element.value, 10);
-  });
-
-  total1 = n1 * medidas.media1;
-
-  Array.from(elements2).forEach((element, index) => {
-    n2 += parseInt(element.value, 10);
-   
-  });
-  total2 = n2 * medidas.medida2;
-
-  soma = total1 + total2;
-  return soma;
+function plus(idx) {
+    let medida = this.medidas[idx];
+    medida.amount = Number((medida.amount + 1).toFixed(1));
+    this.change(idx);
 }
 
-
-function calculo(num) {
-  document.querySelector('.resultado').innerHTML = calc_elementos().toFixed(2);
-  document.querySelector('.qtde').innerHTML = (calc_elementos() + num).toFixed(2);
+function down(idx) {
+    let medida = this.medidas[idx];
+    if (medida.amount > 0) {
+        medida.amount = Number((medida.amount - 1).toFixed(1));
+    }
+    this.change(idx);
 }
+
+function calc() {
+    var soma = 0;
+    for (const medida of this.medidas) {
+        soma = Number((soma + (medida.value * medida.amount)).toFixed(1));
+    }
+    document.getElementById('result').innerHTML = soma;
+}
+
+function change(idx) {
+    let input = this.inputs.item(idx)
+    input.value = medidas[idx].amount;
+    this.calc();
+}
+
+function createElements(){
+    let itens = document.getElementsByClassName('itens')[0];
+    for (let idx = 0; idx < this.medidas.length; idx++) {
+        let div = document.createElement("div");
+        div.classList.add("number-input");
+        let down = document.createElement("button");
+        down.setAttribute("onclick", `down(${idx})`);
+        down.classList.add('down');
+        let plus = document.createElement("button");
+        plus.setAttribute("onclick", `plus(${idx})`);
+        plus.classList.add('plus');
+        let input = document.createElement('input');
+        input.setAttribute("onchange", `change(${idx})`)
+        input.setAttribute("min", "0")
+        input.setAttribute("name", `valor${idx + 1}`)
+        input.setAttribute("type", "number")
+        input.setAttribute("value", "0")
+        input.classList.add("valor")
+        div.appendChild(down)
+        div.appendChild(input)
+        div.appendChild(plus)
+        itens.appendChild(div);
+    }
+    this.inputs = document.getElementsByClassName('valor');
+}
+
+document.onload = (this.createElements());
