34337fe4328a2025f97541e8bf952be3d4503bfc
F.Carriel <fc.felipecarriel@gmail.com>
2021-01-12 00:20:47 -0300

    reducing html, adjusting navigation and refactoring the code

--- js/calculador.js
+++ js/calculador.js
@@ -1,64 +1,181 @@
+
+var comodos = {
+    salon: [
+        { name: "Meuble TV", imgSrc: "meubles-tv.png", value: 1, amount: 0 },
+        { name: "TV", imgSrc: "tv.png", value: 1, amount: 0 },
+        { name: "Grand TV", imgSrc: "tv grand.png", value: 1.1, amount: 0 },
+        { name: "Lustre", imgSrc: "lustre.png", value: 0.2, amount: 0 },
+        { name: "Meuble hifi", imgSrc: "meuble-hifi.png", value: 0.5, amount: 0 },
+        { name: "Fauteuil", imgSrc: "FAUTEUIL.png", value: 0.5, amount: 0 },
+        { name: "Canapé 3 places", imgSrc: "canape 3 places.png", value: 3, amount: 0 },
+        { name: "Canapé 2 places", imgSrc: "canape 2 places.png", value: 2, amount: 0 },
+        { name: "Buffet", imgSrc: "buffet.png", value: 2, amount: 0 },
+        { name: "Piano", imgSrc: "piano.png", value: 1.5, amount: 0 },
+        { name: "Table basse", imgSrc: "table-basse.png", value: 1.1, amount: 0 },
+        { name: "Chaise", imgSrc: "chaise.png", value: 0.3, amount: 0 },
+        { name: "Bibliotèque", imgSrc: "bibliotheque.png", value: 1, amount: 0 },
+        { name: "Cadre", imgSrc: "cadre-photo.png", value: 0.2, amount: 0 },
+    ],
+    cuisine: [
+        { name: "Meuble haut", imgSrc: "hightcabinet.png", value: 0, amount: 0 },
+        { name: "Table", imgSrc: "table.png", value: 0, amount: 0 },
+        { name: "Chaise", imgSrc: "chaise.png", value: 0, amount: 0 },
+        { name: "Réfrigérateur", imgSrc: "refrigerateur.png", value: 0, amount: 0 },
+        { name: "Congélateur", imgSrc: "congelateur.png", value: 0, amount: 0 },
+        { name: "Lave-linge", imgSrc: "LAVE-LINGE.png", value: 0, amount: 0 },
+        { name: "Lave-vaisselle", imgSrc: "LAVE-VAISSELLE.png", value: 0, amount: 0 },
+        { name: "Casseroles, Poeles", imgSrc: "CASSEROLES, POELES.png", value: 0, amount: 0 },
+        { name: "Plaques de cuisson", imgSrc: "PLAQUES DE CUISSON.png", value: 0, amount: 0 },
+        { name: "Micro-ondes", imgSrc: "microondas.png", value: 0, amount: 0 },
+        { name: "Four", imgSrc: "four.png", value: 0, amount: 0 },
+        { name: "Cuisinière", imgSrc: "cuisiniere.png", value: 0, amount: 0 },
+        { name: "Meuble bas", imgSrc: "lowcabinet1.png", value: 0, amount: 0 }
+    ],
+    chambre: [
+        { name: "Lit 2 places", imgSrc: "LIT 2 PLACES.png", value: 0, amount: 0 },
+        { name: "Penderie", imgSrc: "PENDERIE1.png", value: 0, amount: 0 },
+        { name: "Miroir (glace)", imgSrc: "mirror.png", value: 0, amount: 0 },
+        { name: "Chiffonier", imgSrc: "chiffonier.png", value: 0, amount: 0 },
+        { name: "Blibliothèque", imgSrc: "bibliotheque.png", value: 0, amount: 0 },
+        { name: "Commode", imgSrc: "commode.png", value: 0, amount: 0 },
+        { name: "Armoire", imgSrc: "ARMOIRE.png", value: 0, amount: 0 },
+        { name: "Chevet", imgSrc: "chevet.png", value: 0, amount: 0 },
+        { name: "Lit superposé", imgSrc: "lit-superpose.png", value: 0, amount: 0 },
+        { name: "Mezzanine", imgSrc: "mezzanine.png", value: 0, amount: 0 },
+        { name: "Lit 1 place", imgSrc: "LIT 1 PLACE.png", value: 0, amount: 0 },
+        { name: "Secrétaire", imgSrc: "secretary.png", value: 0, amount: 0 },
+    ],
+    bureau: [
+        { name: "Bureau", imgSrc: "bureau.png", value: 0, amount: 0 },
+        { name: "Chaise", imgSrc: "chaise-de-bureau.png", value: 0, amount: 0 },
+        { name: "Meuble de rangement", imgSrc: "MEUBLE DE RANGEMENT.png", value: 0, amount: 0 },
+        { name: "Bibliothèque", imgSrc: "BIBLIOTHÈQUE.png", value: 0, amount: 0 },
+        { name: "Ordinateur", imgSrc: "ordinateur.png", value: 0, amount: 0 },
+        { name: "Fax", imgSrc: "fax.png", value: 0, amount: 0 },
+        { name: "Imprimante", imgSrc: "imprimante.png", value: 0, amount: 0 },
+    ],
+    divers: [
+        { name: "Parasol", imgSrc: "parasol.png", value: 0, amount: 0 },
+        { name: "Aspirateur", imgSrc: "aspirateur.png", value: 0, amount: 0 },
+        { name: "Carton livre", imgSrc: "CARTON LIVRE.png", value: 0, amount: 0 },
+        { name: "Carton standard", imgSrc: "CARTON STANDARD.png", value: 0, amount: 0 },
+        { name: "Chaine WiFi", imgSrc: "chaine-wifi.png", value: 0, amount: 0 },
+        { name: "Livres", imgSrc: "livre.png", value: 0, amount: 0 },
+        { name: "CD, DVD", imgSrc: "cddvd.png", value: 0, amount: 0 },
+        { name: "Miroirs", imgSrc: "mirrors.png", value: 0, amount: 0 },
+        { name: "Lampe", imgSrc: "lampe.png", value: 0, amount: 0 },
+        { name: "Établi", imgSrc: "Établi.png", value: 0, amount: 0 },
+        { name: "Chaise de jardin", imgSrc: "CHAISE DE JARDIN.png", value: 0, amount: 0 },
+        { name: "Poussette de bebe", imgSrc: "poussette-de-bebe.png", value: 0, amount: 0 },
+        { name: "Tondeuse a gazon", imgSrc: "tondeuse-a-gazon.png", value: 0, amount: 0 },
+        { name: "Transat", imgSrc: "transat.png", value: 0, amount: 0 },
+        { name: "Velo", imgSrc: "velo.png", value: 0, amount: 0 },
+    ]
+}
+
+var comodosNames = Object.keys(comodos);
+var currentComodo = comodosNames[0];
 var inputs;
-var medidas = [
-    { value: 1, amount: 0 },
-    { value: 0.6, amount: 0 },
-    { value: 1.1, amount: 0 },
-    { value: 0.2, amount: 0 },
-    { value: 0.5, amount: 0 }
-]
 
 function plus(idx) {
-    let medida = this.medidas[idx];
-    medida.amount = Number((medida.amount + 1).toFixed(1));
+    let item = this.comodos[currentComodo][idx];
+    item.amount = Number((item.amount + 1).toFixed(1));
     this.change(idx);
 }
 
 function down(idx) {
-    let medida = this.medidas[idx];
-    if (medida.amount > 0) {
-        medida.amount = Number((medida.amount - 1).toFixed(1));
+    let item = this.comodos[currentComodo][idx];
+    if (item.amount > 0) {
+        item.amount = Number((item.amount - 1).toFixed(1));
     }
     this.change(idx);
 }
 
 function calc() {
     var soma = 0;
-    for (const medida of this.medidas) {
-        soma = Number((soma + (medida.value * medida.amount)).toFixed(1));
+    for (const comodo of comodosNames) {
+        for (const item of this.comodos[comodo]) {
+            soma = Number((soma + (item.value * item.amount)).toFixed(1));
+        }
     }
     document.getElementById('result').innerHTML = soma;
 }
 
 function change(idx) {
     let input = this.inputs.item(idx)
-    input.value = medidas[idx].amount;
+    input.value = comodos[currentComodo][idx].amount;
     this.calc();
 }
 
-function createElements(){
-    let itens = document.getElementsByClassName('itens')[0];
-    for (let idx = 0; idx < this.medidas.length; idx++) {
-        let div = document.createElement("div");
-        div.classList.add("number-input");
+function createElements(comodo) {
+    this.currentComodo = comodo;
+    document.getElementById('comodos').innerHTML = "";
+    for (const item of this.comodos[currentComodo]) {
+        const idx = this.comodos[currentComodo].indexOf(item);
+
+
+        let article = document.createElement("article");
+        article.classList.add('itens');
+
+        let imgDiv = document.createElement("div");
+        imgDiv.classList.add('imagem');
+
+        let imgTitle = document.createElement("h6");
+        imgTitle.innerHTML = item.name.toUpperCase();
+
+        let img = document.createElement("img");
+        img.id = 'img';
+        img.src = `./img/${comodo}/${item.imgSrc}`;
+        imgDiv.appendChild(imgTitle);
+        imgDiv.appendChild(img);
+        article.appendChild(imgDiv);
+
+        let numberDiv = document.createElement("div");
+        numberDiv.classList.add("number-input");
+
         let down = document.createElement("button");
         down.setAttribute("onclick", `down(${idx})`);
         down.classList.add('down');
-        let plus = document.createElement("button");
-        plus.setAttribute("onclick", `plus(${idx})`);
-        plus.classList.add('plus');
+
         let input = document.createElement('input');
         input.setAttribute("onchange", `change(${idx})`)
         input.setAttribute("min", "0")
         input.setAttribute("name", `valor${idx + 1}`)
         input.setAttribute("type", "number")
-        input.setAttribute("value", "0")
+        input.setAttribute("value", item.amount)
         input.classList.add("valor")
-        div.appendChild(down)
-        div.appendChild(input)
-        div.appendChild(plus)
-        itens.appendChild(div);
+
+        let plus = document.createElement("button");
+        plus.setAttribute("onclick", `plus(${idx})`);
+        plus.classList.add('plus');
+
+        numberDiv.appendChild(down)
+        numberDiv.appendChild(input)
+        numberDiv.appendChild(plus)
+        article.appendChild(numberDiv);
+
+        document.getElementById('comodos').appendChild(article);
     }
+
+
+
     this.inputs = document.getElementsByClassName('valor');
 }
 
-document.onload = (this.createElements());
+function createButtons() {
+    let container = document.getElementsByClassName('multi-button')[0];
+    for (const comodo of comodosNames) {
+        let button = document.createElement('button');
+        button.setAttribute("onclick", `createElements('${comodo}')`);
+        let a = document.createElement('a');
+        a.setAttribute("id", 'link');
+        a.innerHTML = comodo.toUpperCase();
+        button.appendChild(a);
+        container.appendChild(button);
+    }
+}
+
+document.onload = (
+    createButtons(),
+    createElements(currentComodo)
+);
